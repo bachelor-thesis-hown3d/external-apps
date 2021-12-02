afd74bab2ab67926e77917a40df1dfde9af809ba
hown3d <ludi.origin@gmail.com>
2021-12-02 13:36:53 +0100

    designate

---
+++ designate/manager/setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+  
+until designate-manage database sync; do
+  >&2 echo "Mysql is unavailable - sleeping"
+  sleep 1
+done
+
+until designate-manage pool update --file /var/lib/kolla/config_files/manager/pools.yaml; do
+  >&2 echo "Pool update not yet possible - sleeping"
+  sleep 1
+done
